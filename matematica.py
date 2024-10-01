7636433b920e408f6283d44cd958b6085326bb08
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2024-10-01 14:31:19 +0000

    matematica finalizada

--- matematica.py
+++ matematica.py
@@ -6,3 +6,7 @@ def volume(largura, profundidade, altura):
 
 print(area(2,3))
 print(volume(2,3,5))
+
+largura = input('Forneça a largura da área: ')
+profundidade = input('Forneça a profundidade da área: ')
+print(area(int(largura),int(profundidade)))
