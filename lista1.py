8c78460d3ae8296987aadea60837c5850ac286e7
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2024-10-04 12:13:12 +0000

    q1-q10

--- lista1.py
+++ lista1.py
@@ -3,29 +3,64 @@ Exercícios sobre os comandos básicos em Python
 '''
 
 #1. Faça um programa que imprima o seu nome.
+def q1():
+    print('João Paulo')
 
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
+def q2():
+    print(30*27)
 
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+def q3():
+    print(f'Média: {round((5+8+12)/3,1)}')
 
 #4. Faça um programa que leia e imprima um número inteiro.
+def q4():
+    numero  = int(input('Digite um número inteiro: '))
+    print(numero)
 
 #5. Faça um programa que leia dois números reais e os imprima.
+def q5():
+    num1 = float(input('Digite o primeiro número: '))
+    num2 = float(input('Digite o segundo número: '))
+    print(f'{num1}\n{num2}')
 
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
+def q6():
+    numero = int(input('Digite um número: '))
+    print(f'Antecessor de {numero}: {numero-1}')
+    print(f'Sucessor de {numero}: {numero+1}')
 
 #7. Faça um programa que leia o nome o endereço e o telefone de
 #   um cliente e ao final, imprima esses dados.
+def q7():
+    nome = input('Nome: ')
+    endereco = input('Endereço: ')
+    telefone = input('Telefone: ')
+    print(f'{nome}\t{endereco}\t{telefone}')
 
 #8. Faça um programa que leia dois números inteiros e imprima a
 #   subtração deles.
+def q8():
+    num1 = int(input('Número 1: '))
+    num2 = int(input('Número 2: '))
+    print(f'{num1} - {num2} = {num1-num2}')
 
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
+def q9():
+    numero = float(input('Número: '))
+    print(f'¼ de {numero} = {round(numero/4,2)}')
 
 #10. Faça um programa que leia três números reais e calcule a
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
+def q10():
+    num1 = float(input('Número 1: '))
+    num2 = float(input('Número 2: '))
+    num3 = float(input('Número 3: '))
+    media = (num1+num2+num3)/3
+    print(f'Média: {round(media,2)}')
 
 #11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
