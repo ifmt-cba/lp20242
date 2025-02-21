3e767521bef876e84e2649972a4eb2b22089bda9
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2025-02-21 07:59:06 -0400

    pf.py

---
+++ pf.py
@@ -0,0 +1,30 @@
+'''
+PROVA FINAL DE PYTHON
+Ao término enviar e-mail conforme modelo:
+Para:       preti.joao@ifmt.edu.br
+Assunto:    Prova Final de Laboratório de Programação 2024/2
+Mensagem:   NOME COMPLETO DO ESTUDANTE
+Anexo:      pf.py
+'''
+
+#1. Faça um programa que efetue a apresentação do valor da conversão
+#    em real (R$) de um valor lido em dólar (US$). Para isso, será
+#    necessário também ler o valor da cotação do dólar.
+#    Tratamento de exceções/erros deve ser realizado para impedir
+#    valores alfanuméricos em uma variável numérica.(2,5pt)
+
+#2. Faça um programa que leia um número e imprima uma das mensagens:
+#   "Maior do que 20", "Igual a 20"ou "Menor do que 20". (2,5pt)
+
+#3. Faça umprograma que leia um número inteiro e apresente o fatorial desse
+#   número.
+#   Ex.: o fatorial de 5 é igual a 5x4x3x2x1 = 120. (2,5pt)
+
+#4. Faça um programa que permita:
+#   * ler um arquivo (licencas.dat) contendo dados de vários softwares (nome do software, número da licença);
+#   * buscar a licença de um software pelo nome;
+#   Os dados no arquivo (licencas.dat) estão organizados conforme:
+#        NOME_SOFTWARE;NUMERO_LICENCA
+#        NOME_SOFTWARE;NUMERO_LICENCA
+#        ...
+#   (2,5pt)
